8468760d6248f2d715a6aa8ce861003b213d2fa9
liucuilv <zeyechf@gmail.com>
2019-07-16 22:22:24 +0800

    修改群聊管理模块

---
+++ src/components/Table.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="table-container">
+      <el-table
+        :data="tableData"
+        v-loading="listloading"
+        style="width:100%"
+        border>
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+        label="地址"
+        width="180">
+
+        </el-table-column>
+      </el-table>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Table",
+      data(){
+          return{
+            tableData: [{
+              date: '2016-05-02',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+              date: '2016-05-04',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+              date: '2016-05-01',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+              date: '2016-05-03',
+              name: '王小虎',
+              address: '上海市普陀区金沙江路 1516 弄'
+            }]
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
